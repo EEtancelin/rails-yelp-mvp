8fcac32f9b17f8eecf1984d1151f653c491bcd0b
EEtancelin <etancelin.edouard@gmail.com>
2016-10-12 12:04:54 +0200

    Generate standard routes and generate controller action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
 end
